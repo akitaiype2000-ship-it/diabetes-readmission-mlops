7f4c1ddbb01d6e1c334c24f73b26fb5f1e05db06
akitaiype2000-ship-it <akitaiypecherian@gmail.com>
2026-09-21 15:44:30 +0530

    Complete production MLOps pipeline

---
+++ api/app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
+app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
+
+@app.get("/")
+def home():
+    return {
+        "message": "Diabetes Readmission Prediction API is running."
+    }
